9be32d1b909cd90d06b508304050700f98c5bb53
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-12-05 23:30:07 +0100

    creation fichier et routes des pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home.jsx';
+import { Logement } from './pages/Logement.jsx';
+import { Error_404 } from './pages/Error_404.jsx';
+import { About } from './pages/About.jsx';
+
 export function App() {
   return (
-    <div>
-    buy bitcoin!
+    <div className='main'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Logement/:id' element={<Logement />} />
+        <Route path='/Error_404' element={<Error_404 />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Error_404.jsx
@@ -0,0 +1,7 @@
+export const Error_404 = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
