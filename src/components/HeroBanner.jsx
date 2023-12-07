fb8cc4455543c39b50e8db47c1f806da482e58ec
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-12-07 10:18:27 +0100

    ajout page about

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,9 +1,17 @@
-export const HeroBanner = () => {
+import PropTypes from 'prop-types';
+
+export const HeroBanner = ({heroTitle, heroImg, src}) => {
   return (
     <section className="hero-banner">
-      <div className="hero-banner__img"></div>
-      <h1 className="hero-banner__title">Chez vous, partout et ailleurs </h1>
+      <div className={`hero-banner__img hero-banner__img-${src}`} style={{backgroundImage: `url(${heroImg})`}}></div>
+      <h1 className="hero-banner__title">{heroTitle}</h1>
       <div className="hero-banner__mask"></div>
     </section>
   );
+};
+
+HeroBanner.propTypes = {
+  heroTitle: PropTypes.string.isRequired,
+  heroImg: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired
 };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,39 @@
+import { HeroBanner } from "../components/HeroBanner";
+import heroImg from "../assets/images/b9995860bb6384a77ca7dc9bf52da3be.jpg";
+import { useState } from "react";
+import { getAllData } from "../services/api/logementApi";
+import { useEffect } from "react";
+import { DropDown } from "../components/DropDown";
+
 export const About = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [valeur, setValeur] = useState([]);
+
+  useEffect(() => {
+    getAllData("../src/db/valeur.json")
+      .then((data) => {
+        setValeur(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  console.log(valeur);
+
+  return (
+    <>
+      <HeroBanner src="about" heroTitle="" heroImg={heroImg} />
+      <div className="valeur">
+        {valeur &&
+          valeur.map((valeur, index) => {
+            return (
+              <div key={index}>
+                <DropDown className="dropDown--lg" title={valeur.title}>
+                  <p className="dropDown__description">{valeur.description}</p>
+                </DropDown>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
 };
