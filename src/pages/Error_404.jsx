033063afb8e1178dc99c7ca6e5cc604617394afa
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-12-07 10:18:55 +0100

    ajout page error 404

--- src/pages/Error_404.jsx
+++ src/pages/Error_404.jsx
@@ -1,7 +1,9 @@
 export const Error_404 = () => {
     return (
-        <div>
-            error 404
+        <div className="error-404">
+            <h1 className="error-404__title">404</h1>
+            <p className="error-404__text">Oups! La page que vous demandez n'sexiste pas.</p>
+            <p className="error-404__text-sm">Retourner sur la page d’accueil</p>
         </div>
     );
 };
