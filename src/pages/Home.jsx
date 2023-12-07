59db80bc103e2fc20ce7337b91cab064fa92c10a
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-12-07 10:22:29 +0100

    ajout props

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { HeroBanner } from "../components/HeroBanner";
 import { Gallery } from "../layouts/Gallery";
+import heroImg from "../assets/images/e270fc8fc902a1eb738458e7b29c1899.jpg";
 
 export const Home = () => {
+    const heroTitle = "Chez vous, partout et ailleurs"
     return ( 
         <>
-            <HeroBanner />
+            <HeroBanner src="home" heroTitle={heroTitle} heroImg={heroImg} />
             <Gallery />
         </>
     );
