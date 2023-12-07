688b09fe65269c75463c9b970832bb5a7ecfd043
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2023-12-07 10:23:19 +0100

    ajout condition pour attendre les donnée

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -8,39 +8,46 @@ import { Rating } from "../components/Rating.jsx";
 import { DropDown } from "../components/DropDown.jsx";
 
 export const Logement = () => {
-    const logementId = useParams(); console.log(logementId);
-    const logements = useContext(LogementsContext);
+  const logementId = useParams();
+  console.log(logementId);
+  const logements = useContext(LogementsContext);
+  console.log(logements);
+  const logement = logements.filter((logement) => {
+    if (logement.id === logementId.id) return logement;
+  })[0];
+  console.log("logement: ", logement);
 
-    const logement = logements[0].filter((logement) => {
-        if (logement.id === logementId.id) return logement;
-    })[0];
-    console.log('logement: ', logement);
-    
-    return (
-        <div className="logement">
-            <Carrousel logement={logement} />
-            <h1 className="logement__title">{logement.title}</h1>
-            <p className="logement__location">{logement.location}</p>
-            <Tags logement={logement} />
-            <Host logement={logement} />
-            <Rating logement={logement} />
-            
-            <div className="logement__details">
-                <DropDown title={"Description"}>
-                    <p className="dropDown__description">{logement.description}</p>
-                </DropDown>
-                <DropDown title={"Equipements"}>
-                    <ul className="dropDown__list">
-                        {logement.equipments.map((equipment, index) => {
-                            return (
-                                <li key={index} className="dropDown__item">
-                                    {equipment}
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </DropDown>
-            </div>
-        </div>
-    );
+  return (
+    <div className="logement">
+      {logement ? (
+        <>
+          <Carrousel logement={logement} />
+          <h1 className="logement__title">{logement.title}</h1>
+          <p className="logement__location">{logement.location}</p>
+          <Tags logement={logement} />
+          <Host logement={logement} />
+          <Rating logement={logement} />
+
+          <div className="logement__details">
+            <DropDown title={"Description"}>
+              <p className="dropDown__description">{logement.description}</p>
+            </DropDown>
+            <DropDown title={"Equipements"}>
+              <ul className="dropDown__list">
+                {logement.equipments.map((equipment, index) => {
+                  return (
+                    <li key={index} className="dropDown__item">
+                      {equipment}
+                    </li>
+                  );
+                })}
+              </ul>
+            </DropDown>
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
